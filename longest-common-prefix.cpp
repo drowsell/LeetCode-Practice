d1c5786223f73abe8f1618d51e0ccc1530cef99e
drowsell <47066061+drowsell@users.noreply.github.com>
2020-11-23 09:00:14 -0800

    Update longest-common-prefix.cpp

--- longest-common-prefix.cpp
+++ longest-common-prefix.cpp
@@ -29,7 +29,7 @@ public:
                 
                 if (tempPrefix == longestPrefix)
                 { 
-                    //break;
+                    break;
                 }
                 else
                 {
