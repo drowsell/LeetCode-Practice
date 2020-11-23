01a8dba50d0da2f2ef18809e9a0f8d4cc4fcb417
drowsell <47066061+drowsell@users.noreply.github.com>
2020-11-23 08:58:59 -0800

    working commit - Created using LeetHub

---
+++ longest-common-prefix.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        string longestPrefix = "";
+        string tempPrefix = "";
+        
+        if (strs.size() == 0)
+            return longestPrefix;
+        
+        int smallestLen = strs[0].length();
+        
+        // Find the shortest word
+        for(int i = 0; i < strs.size(); i++)
+        {
+            if (strs[i].length() < smallestLen)
+                smallestLen = strs[i].length();
+        }
+        
+        // Grab the longest possible prefix
+        longestPrefix = strs[0].substr(0,smallestLen);    
+        
+        // Go through vector checking if all match
+        for(int j = 0; j < strs.size()-1; j++)
+        {
+            for(int k = 0; k <= smallestLen + 1; k++)
+            { 
+                tempPrefix = strs[j+1].substr(0,smallestLen);
+                
+                if (tempPrefix == longestPrefix)
+                { 
+                    //break;
+                }
+                else
+                {
+                    longestPrefix.pop_back();
+                    smallestLen = smallestLen-1;
+                }   
+                cout << longestPrefix << endl;
+            }
+        }
+        
+        return longestPrefix;
+    }
+};
