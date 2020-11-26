055a67ffd1a7951ffe8a988b420a520cd730cab0
drowsell <47066061+drowsell@users.noreply.github.com>
2020-11-26 08:11:34 -0800

    working commit - Created using LeetHub

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        double answer = 0;
+        
+        if(n == 0 || n < 0)
+            return false;
+        else
+            answer = log2(n);
+        
+        if(answer == static_cast<int>(answer))
+            return true;
+        
+        cout << answer << endl;
+        
+        return false;
+    }
+};
